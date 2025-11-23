6286436175a177e1eb0b7b4e87e1c5a10efc169a
AlishaAcache <alisha@acache.co.in>
2025-11-23 23:18:25 +0530

    Update app.py

--- proto2/app.py
+++ proto2/app.py
@@ -211,5 +211,7 @@ def chat():
     short = short_summary(best_excerpt, max_chars=230)
     return jsonify({"answer": short})
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
